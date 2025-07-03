5a53b8a2b5ec3e7b4cb9a48a31a49d7b7bb03682
Benjamin Correa <iambenjamin.developer@gmail.com>
2025-07-02 22:34:44 -0300

    Update/Delete send by email

--- src/Notification/Notification.Worker/Consumers/ProductDeletedConsumer.cs
+++ src/Notification/Notification.Worker/Consumers/ProductDeletedConsumer.cs
@@ -1,5 +1,6 @@
 ﻿using System.Text.Json;
 using MassTransit;
+using Notification.Application.Interfaces;
 using Shared.Contracts.Events;
 
 namespace Notification.Worker.Consumers
@@ -7,10 +8,12 @@ namespace Notification.Worker.Consumers
     public class ProductDeletedConsumer : IConsumer<ProductDeleted>
     {
         private readonly ILogger<ProductDeletedConsumer> _logger;
+        private readonly IEmailService _emailService;
 
-        public ProductDeletedConsumer(ILogger<ProductDeletedConsumer> logger)
+        public ProductDeletedConsumer(ILogger<ProductDeletedConsumer> logger, IEmailService emailService)
         {
             _logger = logger;
+            _emailService = emailService;
         }
 
         public async Task Consume(ConsumeContext<ProductDeleted> context)
@@ -21,6 +24,12 @@ namespace Notification.Worker.Consumers
                 WriteIndented = true
             };
             string payload = JsonSerializer.Serialize(context.Message, options);
+
+            await _emailService.SendEmailAsync(
+                    subject: $"Producto  con Id: {context.Message.Id} eliminado",
+                    bodyHtml: $"<h1>Detalles</h1><p>{payload}</p>"
+                    );
+
 
             _logger.LogInformation($"===ProductDeleted:Consumer===");
             _logger.LogInformation(payload);
--- src/Notification/Notification.Worker/Consumers/ProductUpdatedConsumer.cs
+++ src/Notification/Notification.Worker/Consumers/ProductUpdatedConsumer.cs
@@ -1,5 +1,6 @@
 ﻿using System.Text.Json;
 using MassTransit;
+using Notification.Application.Interfaces;
 using Shared.Contracts.Events;
 
 namespace Notification.Worker.Consumers
@@ -7,10 +8,12 @@ namespace Notification.Worker.Consumers
     public class ProductUpdatedConsumer : IConsumer<ProductUpdated>
     {
         private readonly ILogger<ProductUpdatedConsumer> _logger;
+        private readonly IEmailService _emailService;
 
-        public ProductUpdatedConsumer(ILogger<ProductUpdatedConsumer> logger)
+        public ProductUpdatedConsumer(ILogger<ProductUpdatedConsumer> logger, IEmailService emailService)
         {
             _logger = logger;
+            _emailService = emailService;
         }
 
         public async Task Consume(ConsumeContext<ProductUpdated> context)
@@ -21,6 +24,11 @@ namespace Notification.Worker.Consumers
                 WriteIndented = true
             };
             string payload = JsonSerializer.Serialize(context.Message, options);
+
+            await _emailService.SendEmailAsync(
+                    subject: $"Producto '{context.Message.Name}' actualizado",
+                    bodyHtml: $"<h1>Detalles</h1><p>{payload}</p>"
+                    );
 
             _logger.LogInformation($"===ProductUpdated:Consumer===");
             _logger.LogInformation(payload);
